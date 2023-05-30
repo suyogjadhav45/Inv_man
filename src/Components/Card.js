9bf8cf3bdd9f757dea1148fee9d6e4ebd03d9284
Harsh Pathekar <96983175+pathekarharsh@users.noreply.github.com>
2023-05-30 10:25:30 +0530

    product get request

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
-import "./Card.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
+import "./Card.css";
 
-const Card = ({ title, quantity }) => {
+const Card = ({ id, title, quantity }) => {
   const [Quantity, setQuantity] = useState(quantity);
 
   const increaseQuantity = () => {
@@ -20,9 +21,13 @@ const Card = ({ title, quantity }) => {
       <div className="card-content">
         <h3 className="card-title">{title}</h3>
         <div className="quantity-control">
-          <button className="quantity-button" onClick={decreaseQuantity}>-</button>
+          <button className="quantity-button" onClick={decreaseQuantity}>
+            -
+          </button>
           <p className="card-quantity">{Quantity}Kg in stock</p>
-          <button className="quantity-button" onClick={increaseQuantity}>+</button>
+          <button className="quantity-button" onClick={increaseQuantity}>
+            +
+          </button>
         </div>
         <Link to="/" className="heading">
           <button className="update-button">Update Stock</button>
@@ -32,65 +37,25 @@ const Card = ({ title, quantity }) => {
   );
 };
 
-const cardsData = [
-  {
-    id: 1,
-    title: "Tomato",
-    quantity: "10",
-  },
-  {
-    id: 2,
-    title: "Potato",
-    quantity: "30",
-  },
-  {
-    id: 3,
-    title: "Apple",
-    quantity: "5",
-  },
-  {
-    id: 4,
-    title: "Onion",
-    quantity: "15",
-  },
-  {
-    id: 5,
-    title: "Carrot",
-    quantity: "12",
-  },
-  {
-    id: 6,
-    title: "Tomato",
-    quantity: "10",
-  },
-  {
-    id: 7,
-    title: "Potato",
-    quantity: "30",
-  },
-  {
-    id: 8,
-    title: "Apple",
-    quantity: "5",
-  },
-  {
-    id: 9,
-    title: "Onion",
-    quantity: "15",
-  },
-  {
-    id: 8,
-    title: "Apple",
-    quantity: "5",
-  },
-];
-
 const Cards = () => {
+  const [cardsData, setCardsData] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://example.com/api/products")
+      .then(response => {
+        setCardsData(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="cards">
       {cardsData.map((card) => (
         <Card
           key={card.id}
+          id={card.id}
           title={card.title}
           quantity={card.quantity}
           image={card.image}
