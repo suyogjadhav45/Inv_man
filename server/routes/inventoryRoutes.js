1233a36c73487226bfd322fdb4d9b117ea3d1894
Pratik Vinayak More <C2K20106742@pictsctr.onmicrosoft.com>
2023-06-02 17:22:39 +0530

    fr update

--- server/routes/inventoryRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { updateStock } = require("../controller/inventoryController");
-// const { getStock } = require("../controller/inventoryController");
-
-router.patch("/inventory/:id", updateStock);
-// router.get("/inventory", getStock);
-
-module.exports = router;
